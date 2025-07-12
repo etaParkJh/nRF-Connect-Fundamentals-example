5a31554ede303a8451ceef9d828d5754e6f6b7e0
etaParkJh <parkJh@etaelec.com>
2025-07-12 16:09:05 +0900

    example complete

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 
+#define THREAD_DELAY(x) k_msleep(x)
 int main(void)
 {
-        return 0;
+	while (1) 
+	{
+		printk("Hello World!\n");
+		THREAD_DELAY(1000);
+	}
 }
