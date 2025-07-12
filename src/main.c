e4f6f0f7d749f2d74f00512e16015228c7dc2f97
etaParkJh <parkJh@etaelec.com>
2025-07-12 16:12:39 +0900

    example complete

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include "myfunction.h"
 
 #define THREAD_DELAY(x) k_msleep(x)
+
 int main(void)
 {
-	while (1) 
+	int a = 3, b = 4;
+	while(1)
 	{
-		printk("Hello World!\n");
+		printk("The sum of %d and %d is %d\n", a, b, sum(a,b));
 		THREAD_DELAY(1000);
 	}
 }
