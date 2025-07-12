4bf6a56d805a2d0196be6eb99fc3b6a2ee221c66
etaParkJh <parkJh@etaelec.com>
2025-07-12 16:16:29 +0900

    disable myfunction

--- src/main.c
+++ src/main.c
@@ -1,15 +1,23 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#ifdef CONFIG_MYFUNCTION
 #include "myfunction.h"
+#endif
 
 #define THREAD_DELAY(x) k_msleep(x)
 
 int main(void)
 {
-	int a = 3, b = 4;
-	while(1)
-	{
-		printk("The sum of %d and %d is %d\n", a, b, sum(a,b));
-		THREAD_DELAY(1000);
+
+	while (1) {
+#ifdef CONFIG_MYFUNCTION
+		int a = 3, b = 4;
+		printk("The sum of %d and %d is %d\n", a, b, sum(a, b));
+#else
+		printk("MYFUNCTION not enabled\n");
+		return 0;
+#endif
+		k_msleep(1000);
 	}
+	return 0;
 }
